3ce6b0c42f3ee5dda9a730a83f6c9b6a0a9216e6
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-08-28 17:11:31 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,63 +1,92 @@
 class Translation(object):
-    START_TEXT = """Hello,
-This is a Telegram URL Upload Bot made by @SerialCoIn
+    START_TEXT = """<b>Hai {} </b>,
+a simple Telegram URL Upload Bot!
+it can <b>UPLOAD almost all Direct Links to Telegram as File/Video</b>
+ 
+🚨 Dont Upload PORN video🔞 Links you will Get PERMANENT BAN 🚨
 
-<b>Please send me any Direct download URL link, I can upload to telegram as File/Video</b>
 
-/help for more details..
-
-Support Group : @SerialCoIn
-© @SerialCoIn"""
+┈┈┈••💙✿❤✿💚••┈┈┈
+<b>/help for More Details </b>
+"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
-    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
-URL | filename | username | password"""
+    UPGRADE_TEXT = "you are that much Rich????"
+    FORMAT_SELECTION = """<b>Select the desired format</b> 
+
+🎞  - Stream format (left side)
+📁  - File format (right side)
+
+If you want to set custom thumbnail, send photo 
+or Use auto-generated <a href='{}'>📸</a> <b>Thumbnail.</b>"""
+    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:	
+URL | filename | username | password """
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
+    DOWNLOAD_START = """<b>Downloading to my server 📥</b>
+   
+  <code>Please wait...⏳ 🙇🙇🙇
+  it takes time depend on File Size</code>"""
+    UPLOAD_START = """<b>Yay,File Download Successfully 😊</b>
+    <code>Now Uploading to Telegram 📤</code>"""
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. Join : @SerialCoIn"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @SerialCoIn \nUploaded in {} seconds."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = """**Thank you for Using Me.** 
+    **Now you can send another Link** 
+    @myTestbotz"""
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = "Share and Support\n\n@SerialCoIn"
+    CUSTOM_CAPTION_UL_FILE = " @AnyUrlDLbot"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     CURENT_PLAN_DETAILS = """Current plan details
 --------
-Telegram ID: <code>{}</code>
-Plan name: Free User
-Expires on: 31/12/2020"""
-    HELP_USER = """Hai am URL Uploader bot..
+<b>User Name : {} </b>
+<b>Telegram ID :</b> <code>{}</code>
+<b>Plan name :</b> Free User
+<b>Expires on :</b> 31/12/2020"""
+    HELP_USER = """How to Use me 🤔
     
-1. Send url (Link|New Name with Extension).
-2. Send Custom Thumbnail (Optional).
-3. Select the button.
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File with Screenshots
-   Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots
+1. <b>Send url</b>
+         if you need custom File Name do Like this ☛ (Link|New Name with Extension).
+
+2. <b>Send Custom Thumbnail </b>(Optional).
+
+3. <b>Select the button.</b>
+   <b>SS+Video</b> - File as video with Screenshots
+  <b>SS+File</b>  - File with Screenshots
+   <b>Video</b>  - File as video without Screenshots
+   <b>File</b>  - File without Screenshots
    
-<b>👉 Join For New Tamil Serials :</b> 👉 <a href="https://t.me/SerialCoIn">JOIN</a>
+   thats it, I will Do Rest of it 😌
 
---------
-Send /me to know current plan details
 
-Support Group : @SerialCoIng
-© @SerialCoIng"""
+<b>check /about to Know about this bot</b>
+"""
+    About = """Hi {},
+  
+<b>○ My Name : URLUploader bot
+○ Creator : <a href="https://telegram.dog/oo7robot">This Person</a>
+○ Credits : Everyone in this journey
+○ Language : Python 3
+○ Library : Pyrogram asyncio 
+○ Cloned From : Spechide Source code
+○ Source Code : ☛ <a href="https://github.com">click here</a>
+○ Server : Heroku
+○ Build Status : Beta v2 </b>
+
+"""
+
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
-    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @AnyDLBot"
+    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days."
     FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS [HH:MM:SS]"""
     FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
     FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
@@ -81,3 +110,5 @@ You can use /rename command after receiving file to rename it with custom thumbn
 Free users only 1 request per 30 minutes.
 /upgrade or Try 1800 seconds later."""
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    AUTH_CHANNEL_TEXT = "You must Join Updates Channel to use me."
+    DOWNLOADING = "**AnaliZing ⏳...**"
